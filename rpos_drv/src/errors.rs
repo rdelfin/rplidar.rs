2ab76fd71fb3cb49747c0fb244ac775e503d7c24
Ricardo Delfin <me@rdelfin.com>
2025-06-21 17:42:31 +0100

    Switched error to use thiserror

--- rpos_drv/src/errors.rs
+++ rpos_drv/src/errors.rs
@@ -1,30 +1,24 @@
-pub use failure::{ Fail, Error };
-
-#[derive(Fail, Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum RposError {
     /// The execution of operation failed
-    #[fail(display="operation failed: {}", description)]
-    OperationFail {
-        description: String
-    },
+    #[error("operation failed: {}", description)]
+    OperationFail { description: String },
 
     /// The execution of operation is timed out
-    #[fail(display="operation timeout")]
+    #[error("operation timeout")]
     OperationTimeout,
 
     /// The device doesn't support this operation
-    #[fail(display="operation not support")]
+    #[error("operation not support")]
     OperationNotSupport,
 
     /// The decoding data is invalid according to current protocol
-    #[fail(display="protocol error: {}", description)]
-    ProtocolError {
-        description: String
-    },
+    #[error("protocol error: {}", description)]
+    ProtocolError { description: String },
 
     /// The buffer is too small for message encoding
-    #[fail(display="buffer is too small for message encoding")]
-    BufferTooSmall
+    #[error("buffer is too small for message encoding")]
+    BufferTooSmall,
 }
 
-pub type Result<T> = std::result::Result<T, Error>;
+pub type Result<T> = std::result::Result<T, RposError>;
